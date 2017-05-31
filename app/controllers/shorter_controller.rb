5727e4e76bfe17cd3d510c6b46bec1c6404be417
cardolfo <cardolfo@gmail.com>
2017-05-31 13:35:20 -0300

    Iniciando o encurtamento

--- app/controllers/shorter_controller.rb
+++ app/controllers/shorter_controller.rb
@@ -1,8 +1,18 @@
 class ShorterController < ApplicationController
 
-  
   def index
 
+  end
+
+  def shorter
+    url = shorter_params[:url]
+    redis = Redis.new
+    sid = redis.incr("urls._id").to_s(36)
+    @id = sid.to_s(36)
+  end
+
+  def shorter_params
+    params.require(:shorter).permit(:url)
   end
 
 end
