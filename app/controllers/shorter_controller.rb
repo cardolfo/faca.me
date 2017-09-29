399d560a6b8d23975a2271b0062217de8c8a100d
cardolfo <cardolfo@gmail.com>
2017-09-28 21:00:27 -0300

    Diversos

--- app/controllers/shorter_controller.rb
+++ app/controllers/shorter_controller.rb
@@ -5,19 +5,26 @@ class ShorterController < ApplicationController
   end
 
   def shorter
-    url = shorter_params[:url]
-    hashUrl = Digest::MD5.hexdigest(url)
-    redis = Redis.new
-    unless redis.keys("urls:#{hashUrl}:*").first
-      id = redis.incr("urls._id").to_s(36)
-      key = "urls:#{hashUrl}:#{id}"
-      redis.set(key, url)
+    @url = Url.new
+    @url.initialize(:url_path => shorter_params[:url])
+    puts @url.path
+    if @url.valid?
+      hashUrl = Digest::MD5.hexdigest(url)
+      redis = Redis.new
+      unless redis.keys("urls:#{hashUrl}:*").first
+        id = redis.incr("urls._id").to_s(36)
+        key = "urls:#{hashUrl}:#{id}"
+        redis.set(key, url)
+      else
+        key = redis.keys("urls:#{hashUrl}:*").first
+      end
+      arrayKey = key.split(':')
+      id = arrayKey[2]
+      @shortUrl = "http://faca.me/#{id}"
     else
-      key = redis.keys("urls:#{hashUrl}:*").first
+      @error_message = t('app.messages.not_valid_url')
     end
-    arrayKey = key.split(':')
-    id = arrayKey[2]
-    @shortUrl = "http://faca.me/#{id}"
+
   end
 
   def redirect
@@ -25,15 +32,24 @@ class ShorterController < ApplicationController
     redis = Redis.new
     key = redis.keys("urls:*:#{id}").first
     url = redis.get(key)
-    unless id == "shorter"
-      redirect_to url
-    else
+    puts id
+    if id == "shorter"
       redirect_to "/"
+    else
+      redirect_to url
     end
   end
 
   def shorter_params
-    params.require(:shorter).permit(:url)
+    params.require(:shorter).permit(:path)
+  end
+
+  private
+
+  def valid_url?(uri)
+      uri = URI.parse(uri)
+    rescue URI::InvalidURIError
+      false
   end
 
 end
