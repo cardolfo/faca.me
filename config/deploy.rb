34c513011e3fac7c677a62f975f49deb21eaf962
cardolfo@gmail.com <cadolfo@gmail.com>
2017-10-19 21:31:55 -0300

    Add

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use', 'ruby 2.4.1p111'
+  invoke :'rvm:use', 'ruby-2.4.1'
   command %{ruby --version}
 end
 
